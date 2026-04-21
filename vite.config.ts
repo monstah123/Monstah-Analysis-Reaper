22aa87220211fcd9bde53f4b77b72e1816e7a634
monstah123 <muscle40@gmail.com>
2026-04-21 09:50:54 -0400

    chore: update vite proxy to hit deployed backend for local dev

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://monstah-analysis-reaper.vercel.app',
         changeOrigin: true,
       }
     }
